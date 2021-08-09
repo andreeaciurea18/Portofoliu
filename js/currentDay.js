7304f2719ab53b94acb5adccec5480048f6a1552
andreeaciurea18 <andreeaciurea18@gmail.com>
2021-08-09 20:33:05 +0300

    Adding Files

---
+++ js/currentDay.js
@@ -0,0 +1,6 @@
+var today = new Date();
+var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var dateTime = date + ' ' + time;
+document.getElementById("currentDateTime1").value = dateTime;
+document.getElementById("currentDateTime2").value = dateTime;
